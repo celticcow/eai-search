dab39e449c35bf8157d1a625c462f7aeef1ba7e7
Celticcow <gregory.dunlap@gmail.com>
2020-11-27 11:44:13 -0600

    funcations

--- eai-search.py
+++ eai-search.py
@@ -16,11 +16,44 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 Greg_Dunlap / CelticCow
 """
 
+def search4grp(mds, cma, eai):
+    debug = 1
+    endl = "\n"
+    print("in search4grp")
+
+    user     = "roapi"
+    password = "1qazxsw2"
+
+    sid = apifunctions.login(user, password, mds, cma)
+
+    if(debug == 1):
+        print("session id : " + sid, end=endl)
+    
+
+
+
+
+    ##don't need publish
+    time.sleep(5)
+    logout_result = apifunctions.api_call(mds, "logout", {}, sid)
+    if(debug == 1):
+        print(logout_result, end=endl)
+#end of search4grp
+
 def main():
     debug = 1
     endl = "\n"
 
     print("in main", end=endl)
+
+
+    mds      = "146.18.96.16"
+    cma      = "146.18.96.25"
+    
+
+    eai = "123456"
+
+    search4grp(mds, cma, eai)
 
 if __name__ == "__main__":
     main()
