c338f4c83ebb872947560a5a476807d322844d07
Celticcow <gregory.dunlap@gmail.com>
2020-11-27 08:32:24 -0600

    structure

--- eai-search.py
+++ eai-search.py
@@ -1,4 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 -W ignore::DeprecationWarning
 
+import requests
+import json
+import sys
+import time
+import ipaddress
+import apifunctions
+import cgi,cgitb
 
-print("start")
+#remove the InsecureRequestWarning messages
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+"""
+Greg_Dunlap / CelticCow
+"""
+
+def main():
+    debug = 1
+    endl = "\n"
+
+    print("in main", end=endl)
+
+if __name__ == "__main__":
+    main()
+#end of program
